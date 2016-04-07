8bc09247504c39ab309eb3ef97c915862ceb7c18
MyungJin Eun <eunlucy@MyungJins-MacBook-Pro.local>
2016-04-06 18:04:35 -0700

    Proj name changed

--- SimpleCalc/main.swift
+++ SimpleCalc/main.swift
@@ -1,10 +1,11 @@
 //
 //  main.swift
-//  simpleCalc
+//  SimpleCalc
 //
-//  Created by iGuest on 4/4/16.
-//  Copyright © 2016 iGuest. All rights reserved.
+//  Created by MyungJin Eun on 4/6/16.
+//  Copyright © 2016 MyungJin Eun. All rights reserved.
 //
+
 
 import Foundation
 
@@ -18,26 +19,26 @@ if inputArray.count == 1 {
     let input2 = readLine(stripNewline: true)!
     var result = 0
     switch operation {
-        case "+":
-            print("result: \(input1 + input2)")
-        case "-":
-            print("result: \(input1 - input2)")
-        case "*":
-            print("result: \(input1 * input2)")
-        case "/":
-            print("result: \(input1 / input2)")
-        default:
-            print("Meh")
+    case "+":
+        print("result: \(input1 + input2)")
+    case "-":
+        print("result: \(input1 - input2)")
+    case "*":
+        print("result: \(input1 * input2)")
+    case "/":
+        print("result: \(input1 / input2)")
+    default:
+        print("Meh")
     }
-
+    
 } else {
     operation = inputArray[inputArray.count - 1]
     inputArray.removeLast()
 }
 
 switch operation {
-    case "+":
-        result =
+case "+":
+    result =
 }
 
 
